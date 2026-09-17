41c62564ec5bd076800ffeaed23835b620b4a4bb
DaniloSStocco <danilosalmen@gmail.com>
2026-09-17 12:27:36 -0300

    FUNC1

--- source/registro.c
+++ source/registro.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "registro.h"
 #include "fornecidas.h"
+#include "sep.h"
 
 #define TAM_REG ((sizeof(int)*4) + (sizeof(char)*2))
 #define TAM_CAB ((sizeof(int)*4) + sizeof(char))
@@ -47,11 +48,11 @@ void lerCabecalho(Cabecalho *cab, FILE *arquivoBin)
 
 void escreverCabecalho(Cabecalho *cab, FILE *arquivoBin)
 {
-    fread(&cab->status, sizeof(char), 1, arquivoBin);
-    fread(&cab->topoPilha, sizeof(int), 1, arquivoBin);
-    fread(&cab->proxRRN, sizeof(int), 1, arquivoBin);
-    fread(&cab->nroRegRem, sizeof(int), 1, arquivoBin);
-    fread(&cab->nroPares, sizeof(int), 1, arquivoBin);
+    fwrite(&cab->status, sizeof(char), 1, arquivoBin);
+    fwrite(&cab->topoPilha, sizeof(int), 1, arquivoBin);
+    fwrite(&cab->proxRRN, sizeof(int), 1, arquivoBin);
+    fwrite(&cab->nroRegRem, sizeof(int), 1, arquivoBin);
+    fwrite(&cab->nroPares, sizeof(int), 1, arquivoBin);
 }
 
 void voltaUmRegistro(FILE *arquivoBin){
@@ -76,6 +77,7 @@ void imprimirRegistro(Registro reg){
 
 void excluirRegistro(Registro *reg, Cabecalho *cab, FILE* arqBin)
 {
+    
     //escrever RRN no topo pilha
     fseek(arqBin, 1, SEEK_SET);
     fwrite(&reg->RRN, sizeof(int), 1, arqBin);
@@ -214,14 +216,62 @@ void buscaRegistro(FILE *arquivoBin, int opcao){
 
 void FUNC1(char *NomeArquivoEntrada, char*NomeArquivoBin){
     FILE *arqCsv = fopen(NomeArquivoEntrada, "r");
-    FILE *arqBin = fopen(NomeArquivoBin, "rb+");
-
+    
     if((arqCsv == NULL)){
         printf("Falha no processamento do arquivo.");
         return;
     }
+    FILE *arqBin = fopen(NomeArquivoBin, "wb"); //somente escreve, cria arquivo
     
-    
+    Cabecalho cab;
+    cab.status = '1';
+    cab.topoPilha = -1;
+    cab.proxRRN = 0;
+    cab.nroRegRem = 0;
+    cab.nroPares = 0;
+
+    escreverCabecalho(&cab, arqBin); //Ainda não está correto
+
+    char temp[200]; //buffer pra andar no csv com tamanho seguro pra 1 linha do csv
+    char *linha, *token; //token pro meu_strsep
+
+    fgets(temp, sizeof(temp), arqCsv); //pula a primeira linha
+
+    while(fgets(temp, sizeof(temp), arqCsv)){
+        linha = temp;
+        Registro reg;
+
+        temp[strcspn(temp, "\r\n")] = 0; //transforma em nulo (codigo 0) o caractere q pula linha
+
+        //idPoPs
+        token = meu_strsep(&linha, ","); //encontra o texto entre vírgulas (garantido no csv)
+        reg.idPoPs = atoi(token);
+
+        //idConectaPoPs
+        token = meu_strsep(&linha, ",");
+        reg.idPoPsConectado = atoi(token);
+
+        //velocidade
+        token = meu_strsep(&linha, ",");
+        reg.velocidade = atoi(token);
+
+        //unidadeMedida
+        token = meu_strsep(&linha, ",");
+        reg.unidadeMedida = (meu_strsep(&linha, ","))[0];
+
+        //removido
+        reg.removido = '0';
+
+        //encadeamentoPilha
+        reg.tamanhoPilha = -1;
+
+        cab.proxRRN++;
+        cab.nroPares++;
+
+        escreverRegistro(&reg, arqBin);
+    }
+    fseek(arqBin, 0, SEEK_SET);
+    escreverCabecalho(&cab, arqBin);
 
     fclose(arqCsv);
     fclose(arqBin);
